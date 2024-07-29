6c011039cbf563d567a568e06e4e6d6f71d83fcc
Bruno <bds.rodrigues@icloud.com>
2024-07-29 20:26:55 -0300

    Add vesiculo

--- src/views/app/LandingView.vue
+++ src/views/app/LandingView.vue
@@ -1,29 +1,15 @@
 <template>
   <HeaderSection />
   <LiveCountdown />
+  <ContentSection variant="primary">
+    <p>
+      “Deus mudou o teu caminho até juntares com o meu e guardou a tua vida separando-a para mim. Para onde fores, irei.
+      Onde tu repousares, repousarei. Teu Deus será o meu Deus. Teu caminho o meu será.” – Rute 1:16-17
+    </p>
+  </ContentSection>
+  <ContentSection variant="secondary" title="O casal"> </ContentSection>
   <ContentSection variant="primary" title="NOVAS INFORMAÇÕES EM BREVE!" />
-  <!-- <ContentSection variant="primary">
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tincidunt rutrum erat ut placerat. In in vestibulum
-      augue, at lacinia mi. Curabitur magna eros, venenatis eu orci ac, tempor commodo odio. Nam sed justo pellentesque,
-      gravida ante vitae, aliquet felis. Duis quis accumsan sapien. Pellentesque at congue arcu, at volutpat diam.
-      Aenean sit amet erat eget neque auctor dictum vitae vitae ipsum. Pellentesque habitant morbi tristique senectus et
-      netus et malesuada fames ac turpis egestas. Fusce feugiat mollis velit, non ornare erat pulvinar sit amet. Nam at
-      mi nunc. Nam eu bibendum nulla, sed tempor tortor. Suspendisse et consequat neque, in finibus purus. Class aptent
-      taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse pretium interdum mi,
-      sed accumsan lectus molestie id. Vivamus vel rhoncus lacus, ut varius dolor. Nunc non sem enim.
-    </p>
-    <p>
-      Mauris sit amet velit id nisl placerat sodales ac at augue. Aliquam erat volutpat. Ut eu ullamcorper tortor.
-      Vestibulum congue felis dolor, nec sodales elit tristique vitae. Fusce laoreet condimentum risus at elementum. In
-      egestas rutrum tortor, vel maximus diam iaculis et. Cras tortor massa, scelerisque vitae enim quis, facilisis
-      luctus tortor. Vivamus ultrices, quam accumsan euismod suscipit, ipsum orci euismod justo, sit amet suscipit nunc
-      urna at lacus. Vestibulum blandit rutrum ligula a ullamcorper. Sed vitae libero consequat, fringilla nisl sed,
-      laoreet arcu.
-    </p>
-  </ContentSection> -->
-  <!-- <ContentSection variant="secondary" title="O casal" />
-  <ContentSection variant="primary" title="Cerimônia" />
+  <!-- <ContentSection variant="primary" title="Cerimônia" />
   <ContentSection variant="secondary" title="Recepção" /> -->
 </template>
 
